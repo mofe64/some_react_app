83528715f81a335ddc7cedbfd4d395420a48bd81
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-05-13 15:34:02 +0100

    component impl

---
+++ src/Male.js
@@ -0,0 +1,9 @@
+const Male = ({ color, height, weight }) => {
+    return (
+        <div>
+            <p>color: {color}</p>
+            <p>height: {height} ft</p>
+            <p>weight: {weight} kg</p>
+        </div>
+    )
+}
